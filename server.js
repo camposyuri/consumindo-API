c028d8e1e61a4379362886a04ae4eba9b7e35208
camposyuri <yurinapoleao@gmail.com>
2021-01-21 20:00:51 -0300

    Created API

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ messsage: "Okay" });
+  return response.json([{ name: "John" }, { name: "Tony" }]);
 });
 
 app.listen(3333, () => {
